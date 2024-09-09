1c9b42ff246111f90fb1a1010f96a47dac77cdef
eziz-tajiev <eziztajiev@gmail.com>
2024-09-09 22:22:45 +0500

    moved on the figures

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,6 +7,26 @@ const Hero = () => {
         <div className="hero">
             <div className="left-h">
                 <Header />
+                {/* the-best */}
+                <div className="the-best">
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+                {/* Hero heading */}
+                <div className="hero-text">
+                     <div>
+                        <span className="stroke-text">Shape </span>
+                        <span>Your</span>
+                     </div>
+                     <div>
+                        <span>Ideal body</span>
+                     </div>
+                     <div>
+                        <span>
+                            In here we will help you to shape and build your ideal body and live up your live to fullest
+                        </span>
+                    </div>
+                </div>
             </div>
             <div className="right-h">right side</div>
         </div>
