44103bf85924ecdf105098b219aa374833374944
eziz-tajiev <eziztajiev@gmail.com>
2024-09-10 22:00:00 +0500

    stop in calories

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import './Hero.css'
 import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+
+
+
 
 const Hero = () => {
     return (
@@ -49,7 +56,24 @@ const Hero = () => {
                 </div>
 
             </div>
-            <div className="right-h">right side</div>
+            <div className="right-h">
+                <button className="btn">Join now</button>
+                <div className="heart-rate">
+                    <img src={Heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+                {/* Hero images */}
+                <img className="hero-image" src={hero_image} alt="" />
+                <img className="hero-image-back" src={hero_image_back} alt="" />
+                {/* Calories */}
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <span>Calories Burned</span>
+                    <span>220 kcal</span>
+                </div>
+            </div>
         </div>
     )
 }
