e551ab95a7d86cf40cf9fc6a4e34011463e271bd
eziz-tajiev <eziztajiev@gmail.com>
2024-09-22 12:01:27 +0500

    start the last page that call testimonials

---
+++ src/components/Testimonials/Testimonials.tsx
@@ -0,0 +1,16 @@
+import './Testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div className="Testimonials">
+      <div className="left-t">
+        <span>Testimonials</span>
+        <span className='stroke-text'>What they</span>
+        <span>say about us</span>
+      </div>
+      <div className="right-t"></div>
+    </div>
+  )
+}
+
+export default Testimonials
