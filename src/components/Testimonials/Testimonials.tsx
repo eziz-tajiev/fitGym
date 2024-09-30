3f6b532a6d5dc4d43dfca0846235784196dcd77d
eziz-tajiev <eziztajiev@gmail.com>
2024-09-30 23:16:11 +0500

    finished hamburger

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -11,11 +11,12 @@ const Testimonials = () => {
   const tLength = testimonialsData.length;
 
   return (
-    <div className="Testimonials">
+    <div className="testimonials">
       <div className="left-t">
         <span>Testimonials</span>
         <span className='stroke-text'>What they</span>
         <span>say about us</span>
+
         <motion.span
         key={selected}
         initial={{opacity: 0, x: 100}}
@@ -24,6 +25,7 @@ const Testimonials = () => {
         transition={transition}>
           {testimonialsData[selected].review}
         </motion.span>
+
         <span>
           <span style={{color: 'var(--orange)'}}>
             {testimonialsData[selected].name}
@@ -31,12 +33,14 @@ const Testimonials = () => {
           - {testimonialsData[selected].status}
         </span>
       </div>
+
       <div className="right-t">
         <motion.div
         initial={{opacity: 0, x: -100}}
         transition={{...transition, duration: 2}}
         whileInView={{opacity: 1, x: 0}}
         ></motion.div>
+
         <motion.div
         initial={{opacity: 0, x: 100}}
         transition={{...transition, duration: 2}}
@@ -50,11 +54,13 @@ const Testimonials = () => {
         exit={{opacity: 0, x: -100}}
         transition={transition}
         src={testimonialsData[selected].image} alt="" />
+
         <div className="arrows">
           <img 
           onClick={()=>
             selected === 0 ? setSelected(tLength - 1) : setSelected((prev)=>prev - 1)}
           src={leftArrow} alt="" />
+          
           <img
           onClick={()=>
             selected === tLength-1 ? setSelected(0) : setSelected((prev)=>prev + 1)}
