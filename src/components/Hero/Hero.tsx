3c79ba9eeb9183fc148674c9287514a1f22a3d99
eziz-tajiev <eziztajiev@gmail.com>
2025-01-16 15:02:43 +0500

    the best is done

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,9 +6,21 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/* The best */}
         <div className="the-best">
           <div></div>
-          <span>the best fitness in the town</span>
+          <span>the best fitness club in the town</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
+            <div>
+              <span></span>
+            </div>
+          </div>
         </div>
         
       </div>
